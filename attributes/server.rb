2dd751fc57512754bb63b1e79dd619b9c7aaf1de
Steffen Gebert <steffen.gebert@typo3.org>
2015-07-21 23:49:52 +0200

    Upgrade server version to 2.4.5

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,4 +1,4 @@
-node.override[:zabbix][:server][:version]           = "2.0.9"
+node.override[:zabbix][:server][:version]           = "2.4.5"
 node.override[:zabbix][:server][:branch]            = "ZABBIX%20Latest%20Stable"
 node.override[:zabbix][:server][:configure_options] = [ "--with-libcurl","--with-net-snmp","--enable-ipv6","--with-jabber=/usr/","--with-ssh2","--with-mysql" ]
 node.override[:zabbix][:web][:fqdn] = "monitoring.typo3.org"
