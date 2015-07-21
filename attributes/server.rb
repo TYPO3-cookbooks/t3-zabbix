93325d3c04db7ac48acd2102a5d880b4edd368eb
Steffen Gebert <steffen.gebert@typo3.org>
2015-07-22 00:43:14 +0200

    No need to override attributes

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,6 +1,5 @@
-node.override[:zabbix][:server][:version]           = "2.4.5"
-node.override[:zabbix][:server][:branch]            = "ZABBIX%20Latest%20Stable"
-node.override[:zabbix][:server][:configure_options] = [ "--with-libcurl","--with-net-snmp","--enable-ipv6","--with-jabber=/usr/","--with-ssh2","--with-mysql" ]
-node.override[:zabbix][:web][:fqdn] = "monitoring.typo3.org"
-node.override[:zabbix][:web_dir] = "/usr/local/zabbix/web"
-
+default[:zabbix][:server][:version]           = "2.4.5"
+default[:zabbix][:server][:branch]            = "ZABBIX%20Latest%20Stable"
+default[:zabbix][:server][:configure_options] = [ "--with-libcurl","--with-net-snmp","--enable-ipv6","--with-jabber=/usr/","--with-ssh2","--with-mysql" ]
+default[:zabbix][:web][:fqdn] = "monitoring.typo3.org"
+default[:zabbix][:web_dir] = "/usr/local/zabbix/web"
