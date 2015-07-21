e01516aa311aeb214cceadfb8e5b555d8dbb455c
Steffen Gebert <steffen.gebert@typo3.org>
2015-07-21 23:32:53 +0200

    Remove DisableHousekeeping option, removed in Zabbix 2.2

--- recipes/partition.rb
+++ recipes/partition.rb
@@ -1,3 +1,5 @@
+# make sure to disable housekeeping in Administration → General → Housekeeper
+
 cron "housekeeping" do
   command "/usr/bin/mysql --skip-column-names -B -h #{node.zabbix.database.dbhost} -u #{node.zabbix.database.dbuser} -p#{node.zabbix.database.dbpassword} #{node.zabbix.database.dbname} -e \"CALL create_zabbix_partitions();\""
   hour 0
