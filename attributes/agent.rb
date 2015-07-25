b3e015611b27c86fb27472bf307d149fd5d9256b
Steffen Gebert <steffen.gebert@typo3.org>
2015-07-25 20:47:35 +0200

    Change attributes from node.override to default

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -1,10 +1,10 @@
-node.override[:zabbix][:agent][:version]        = "2.0.11"
-node.override[:zabbix][:agent][:servers]        = ['monitoring.typo3.org']
-node.override[:zabbix][:agent][:servers_active] = ['monitoring.typo3.org']
-node.override[:zabbix][:agent][:branch]         = "ZABBIX%20Latest%20Stable"
-node.override[:zabbix][:agent][:install_method] = "source"
-node.override[:zabbix][:agent][:include_dir]    = "/usr/local/zabbix/etc/zabbix_agentd.conf.d"
+default[:zabbix][:agent][:version]        = "2.0.11"
+default[:zabbix][:agent][:servers]        = ['monitoring.typo3.org']
+default[:zabbix][:agent][:servers_active] = ['monitoring.typo3.org']
+default[:zabbix][:agent][:branch]         = "ZABBIX%20Latest%20Stable"
+default[:zabbix][:agent][:install_method] = "source"
+default[:zabbix][:agent][:include_dir]    = "/usr/local/zabbix/etc/zabbix_agentd.conf.d"
 
 
 # only for the 2.0.10rc1
-# node.override[:zabbix][:agent][:branch] = "ZABBIX%20Release%20Candidates"
+# default[:zabbix][:agent][:branch] = "ZABBIX%20Release%20Candidates"
