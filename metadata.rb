54312dd6e292f4aab80e8909cedb105241697591
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-12 14:17:35 +0200

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Zabbix configuration"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-
-version          "0.2.4"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends          "build-essential"
 depends          "zabbix", "= 0.0.43"
