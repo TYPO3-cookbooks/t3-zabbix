44f3ea2a4984c1e89cc773f074a8a3052737a221
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-17 20:16:30 +0200

    [FEATURE] Use internal network for monitoring data #major
    Thanks to VPN, we don't need to transfer all monitoring data through
    the public internet (to monitoring.typo3.org).
    
    This change removes the host name for the zabbix agent from the
    cookbook. Instead, this is now defined in the _production_ env.

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -1,6 +1,6 @@
 default[:zabbix][:agent][:version]        = "2.4.6"
-default[:zabbix][:agent][:servers]        = ['monitoring.typo3.org']
-default[:zabbix][:agent][:servers_active] = ['monitoring.typo3.org']
+default[:zabbix][:agent][:servers]        = ['10.186.2.148']
+default[:zabbix][:agent][:servers_active] = ['10.186.2.148']
 default[:zabbix][:agent][:branch]         = "ZABBIX%20Latest%20Stable"
 default[:zabbix][:agent][:install_method] = "source"
 default[:zabbix][:agent][:include_dir]    = "/usr/local/zabbix/etc/zabbix_agentd.conf.d"
